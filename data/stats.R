6efbe25b8b596eec4738a68c5a55b51248556ffb
Matthew Kay <matthew.kay@gmail.com>
2014-02-08 17:12:55 -0800

    fixed lat/long swap

--- data/stats.R
+++ data/stats.R
@@ -9,11 +9,9 @@ library(stringr)
 library(reshape2)
 melt.data.frame = reshape2:::melt.data.frame	#fix conflict of reshape2 with reshape
 
-NICEPLOT_PATH = "../../docs/niceplot/niceplot.R"
 frame_files <- lapply(sys.frames(), function(x) x$ofile)
 frame_files <- Filter(Negate(is.null), frame_files)
 FILE_PATH <- dirname(frame_files[[length(frame_files)]])
-source(paste(FILE_PATH, NICEPLOT_PATH, sep="/"))
 
 DATA_PATH = FILE_PATH
 
@@ -33,6 +31,7 @@ coordinates(df) = c("Latitude", "Longitude")
 proj4string(df) = CRS("+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs")
 df = spTransform(df, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
 df = cbind(df@data, df@coords)
+names(df)[16:17] = c("Longitude", "Latitude")
 
 #write out cleaned data
 write.csv(df, fjoin(DATA_PATH, "whiskies.csv"), row.names=FALSE)
@@ -40,7 +39,7 @@ write.csv(df, fjoin(DATA_PATH, "whiskies.csv"), row.names=FALSE)
 #load map
 md = map_data("world", region="UK")
 
-ggplot(md) + geom_map(aes(map_id=region, alpha=.5), map=md) + geom_point(aes(x=Latitude, y=Longitude, color="red", alpha=Fruity), data=df)
+ggplot(md) + geom_map(aes(map_id=region, alpha=.5), map=md) + geom_point(aes(x=Longitude, y=Latitude, color="red", alpha=Fruity), data=df)
 
 
 #scatterplot matrix of flavors
@@ -48,7 +47,7 @@ plotmatrix(df[,1:12+2]) + geom_jitter() + stat_smooth(method=lm)
 
 #hexbin the flavor characteristics
 binfunc = mean	#binning functions to consider: min, max, mean, median, ... ?
-hb = hexbin(df$Latitude, df$Longitude, IDs=TRUE)
+hb = hexbin(df$Longitude, df$Latitude, IDs=TRUE)
 hexdf = ddply(df, ~ hb@cID, function(d) apply(d[,1:12+2], 2, binfunc))	#aggregate data by bins
 hexdf = cbind(hexdf, hcell2xy(hb))	#put x, y coords of bins into hexdf$x, y
 ggplot(md) + geom_map(aes(map_id=region, alpha=.5), map=md) + geom_hex(aes(x=x, y=y, fill=Medicinal), data=hexdf, stat="identity")
